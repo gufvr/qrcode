5b7c4a104f11786a43f1015aaf2b7c6ae63bc807
GuFvr <gustavo.vfavero@gmail.com>
2024-07-01 08:48:08 -0300

    feat: implement tool selection logic and modularize prompt schema
    - Imported mainPrompt schema from external file.
    - Implemented main function to handle user input and display corresponding messages for QRCode and Password options.
    - Modularized prompt schema into a separate file for better organization.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import prompt from 'prompt'
+import mainPrompt from './prompts/prompt-main.js'
 
-async function Main() {
+async function main() {
+  prompt.get(mainPrompt, async (err, choice) => {
+    if (choice.name == 1) console.log('Você escolheu a opção de QRCode')
+    else if (choice.name == 2) console.log('Você escolheu a opção Password')
+  })
+
   prompt.start()
 }
 
-Main()
+main()
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,5 +1,7 @@
 const mainPrompt = {
   name: 'select',
   description: 'Escolha a ferramenta (1 para QRCode ou 2 para password)',
-  pattern: /^[1-2]+$/,
+  pattern: /^[1-2]$/,
 }
+
+export default mainPrompt
